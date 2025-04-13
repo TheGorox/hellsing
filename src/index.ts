a97f8a5bf62a2c19aa7863cff416358ed79180cc
Sha <gnikita.010@gmail.com>
2025-04-13 21:15:32 +0300

    hotfix: create data folder if not exists

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,32 @@ import { HellsingBot } from './bot/bot.js';
 
 import Log4js from 'log4js';
 import { initDB } from './db/db.js';
+import path from 'path';
+import fs from 'fs';
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const logger = Log4js.getLogger('INDEX');
 logger.level = process.env.LOG_LEVEL || 'info';
 
 async function bootstrap(){
+    initFolders();
     await initDB();
 
     const bot = new HellsingBot(process.env.TELEGRAM_BOT_TOKEN);
     await bot.start();
+}
+
+function initFolders(){
+    const dataPath = path.join(__dirname, '../data');
+    if(!fs.existsSync(dataPath)){
+        fs.mkdirSync(dataPath);
+    }
 }
 
 bootstrap().then(() => {
